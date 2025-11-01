6d839f196af8797273dbf24f75a4e3c436b11697
chanhk-im <cfasd1875@gmail.com>
2025-11-01 15:55:36 +0900

    refactor: exception message 머리말 추가

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,8 +1,9 @@
 package lotto.exception;
 
 public enum ExceptionMessage {
-    NUMBER_IS_NOT_IN_RANGE("범위 바깥의 로또 번호입니다."),
-    NUMBERS_HAS_DUPLICATED_VALUE("중복된 로또 번호가 존재합니다.");
+    NUMBER_IS_NOT_IN_RANGE("[ERROR] 범위 바깥의 로또 번호입니다."),
+    NUMBERS_HAS_DUPLICATED_VALUE("[ERROR] 중복된 로또 번호가 존재합니다."),
+    NUMBERS_HAS_WRONG_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
 
     private final String message;
 
