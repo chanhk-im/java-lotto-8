d2c3430897e67aabe017b789c9bd58c1df84d28a
chanhk-im <cfasd1875@gmail.com>
2025-11-03 11:31:29 +0900

    feat: OutputView 메서드 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.domain.LottoResult;
+
+public final class OutputView {
+    private OutputView() {
+    }
+
+    public static void printInputExpense() {
+
+    }
+
+    public static void printBoughtLottoMessage() {
+
+    }
+
+    public static void printBoughtLottoNumber(List<Integer> numbers) {
+
+    }
+
+    public static void printInptWinningNumbers() {
+
+    }
+
+    public static void printResult(LottoResult result) {
+
+    }
+}
